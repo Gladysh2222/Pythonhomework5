698b88a89f90aba0663999940179b8964bd01c96
Oleg GLadyshev <oleg_gladysh@mail.ru>
2024-05-09 20:48:02 +0300

    Homework5

---
+++ Homework.py
@@ -0,0 +1,18 @@
+"""Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. 
+Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла."""
+
+
+import os
+
+
+def get_absolute_path(path):
+    *_, extension = os.path.splitext(path)
+    path_to, _ = os.path.split(path)
+    file_name = os.path.basename(path).split('.')[0]
+    
+    return path_to, file_name, extension
+
+
+if __name__ == '__main__':
+    path = '/Users/uliagladyseva/Python homework5/Task.txt'
+    print(get_absolute_path(path))
